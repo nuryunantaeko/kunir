c2d1ed53063009e342d10392888542c352b764bc
Guan Indonesia <indonesiaguan@gmail.com>
2019-07-22 03:20:22 +0700

    Fixed bug

--- application/views/templates/__partials/topbar.php
+++ application/views/templates/__partials/topbar.php
@@ -16,24 +16,25 @@
 
 <!-- Topbar Navbar -->
 <ul class="navbar-nav ml-auto">
-
-  <div class="topbar-divider d-none d-sm-block"></div>
-
-  <!-- Nav Item - User Information -->
-  <li class="nav-item dropdown no-arrow">
-    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo ucfirst($this->session->userdata('username')); ?></span>
-      <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-    </a>
-    <!-- Dropdown - User Information -->
-    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-      <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-        Logout
+  <?php $login = $this->session->userdata('login');
+    if ($login): ?>
+    <div class="topbar-divider d-none d-sm-block"></div>
+    
+    <!-- Nav Item - User Information -->
+    <li class="nav-item dropdown no-arrow">
+      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo ucfirst($this->session->userdata('username')); ?></span>
+        <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
       </a>
-    </div>
-  </li>
-
+      <!-- Dropdown - User Information -->
+      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+          Logout
+        </a>
+      </div>
+    </li>
+  <?php endif; ?>
 </ul>
 
 </nav>
